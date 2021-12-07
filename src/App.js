aaaa93afebd85d611bdc1048b4b11ad87143ae1a
adamteddychang <adamchang0725@gmail.com>
2021-12-07 10:32:18 +0100

    initialed react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/nav';
+import Categories from './components/categories';
+import BooksList from './components/bookList';
+import InputBook from './components/inputBook';
+
 
 function App() {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <Router>
+      <div>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element = {<BooksList/>}/>
+        <Route exact path="/input" element = {<InputBook/>}/>
+        <Route path="/categories" element={<Categories/>}/>
+        
+      </Routes>
+      </div>
+    </Router>
+    
   );
 }
 
