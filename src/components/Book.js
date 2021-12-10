ed7feeeb4aae7907a85a8eedff90e9296371c66e
adamteddychang <adamchang0725@gmail.com>
2021-12-10 10:04:06 +0100

    completed book styling

--- src/components/Book.js
+++ src/components/Book.js
@@ -6,7 +6,7 @@ import { CircularProgressbar } from 'react-circular-progressbar';
 import { removeBook } from '../redux/books/books';
 
 const Book = ({ book }) => {
-  const percentage = 66;
+  const percentage = 69;
   const dispatch = useDispatch();
   const deleteBook = (id) => {
     dispatch(removeBook(id));
@@ -25,16 +25,25 @@ const Book = ({ book }) => {
           <button type="submit" className="infoBtns">Edit</button>
         </div>
       </div>
-      <div className="progwithtxt">
-        <div className="progress">
-          <CircularProgressbar value={percentage} />
+      <div className="progress-w-chap">
+        <div className="progwithtxt">
+          <div className="progress">
+            <CircularProgressbar value={percentage} />
+          </div>
+          <div className="progressTXT">
+            <h3 className="percentage">
+              {percentage}
+              %
+            </h3>
+            <p>completed</p>
+          </div>
         </div>
-        <div className="progressTXT">
-          <h3 className="percentage">66%</h3>
-          <p>completed</p>
+        <div className="chapter">
+          <p>CURRENT CHAPTER</p>
+          <h3>Chapter 42</h3>
+          <button type="button">UPDATE PROGRESS</button>
         </div>
       </div>
-
     </div>
   );
 };
